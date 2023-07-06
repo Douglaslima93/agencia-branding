790a432848df0b043da6d54805bddfdf33412cb5
Douglas Lima <douglas07a@gmail.com>
2023-07-06 16:15:03 -0300

    Alteração de estilos

--- src/componentes/Topo/index.jsx
+++ src/componentes/Topo/index.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css'
 
 export default function Topo() {
+
+    const [AlterarTema , setAlterarTema] = useState(true)
+
+    const BotaoTema = () => {
+        setAlterarTema('')
+    }
+
     return(
         <header className="topo">
             <img className="logo" src="./img/logo.png" alt="Logo"/>
-            <button className="botao"><img src="./img/moon.png" alt="Moon"/></button>
+            <button onClick={BotaoTema} className="botao botao-dark"><img src="./img/moon.png" alt="Moon"/></button>
         </header>
     )
 }
